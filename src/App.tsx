b2e7fba9b63ec3eb71425523af04dec6942e0a23
gg <robal.bashar59@gmail.com>
2021-10-16 20:01:33 +0600

    item card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Card } from "./Card";
 import { Input } from "./Input";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 flex items-center flex-col">
-      <div style={{ minWidth: 600 }} className="mt-16">
+      <div style={{ width: 600 }} className="mt-16">
         <h1 className="text-center">TODO</h1>
         <br />
         <Input />
+        <br />
+        <Card />
       </div>
     </div>
   );
---
+++ src/Card.tsx
@@ -0,0 +1,11 @@
+export function Card() {
+  return (
+    <div className="card ">
+      <p className="whitespace-pre-wrap">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce facilisis
+        at ipsum vel rhoncus. Etiam vel turpis id lacus laoreet rutrum at varius
+        lacus. Duis auctor tincidunt nisl eu cursus
+      </p>
+    </div>
+  );
+}
